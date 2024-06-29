d7426a595c2deda5f913b0a9cf67b480190fa1f2
Igor Shipitko <garikrhip20@gmail.com>
2024-06-29 19:44:40 +0300

    мои настроики

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Directions from './components/DataComponents/Directions'
+
+export default function App() {
+  return (
+    <Directions />
+
+  )
+}  
+
---
+++ src/components/DataComponents/Directions.tsx
@@ -0,0 +1,11 @@
+export default function Directions() {
+    return (
+      <div>
+        <div>
+          <h1>Направления</h1>
+          <h2>Давно хотели попробовать йогу, но не решались начать</h2>
+        </div>
+      </div>
+    );
+  }
+  
