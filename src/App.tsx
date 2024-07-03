70a6bced47c294bec872b4e24394e550e87e61be
Igor Shipitko <garikrhip20@gmail.com>
2024-07-03 18:39:16 +0300

    dev2

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,12 @@ import UserProfilePage from "./pages/UserProfilePage";
 import WorkoutVideoPage from "./pages/WorkoutVideoPage";
 
 export default function App() {
-    return (
-        <Routes>
-            <Route path={appRoutes.MAIN} element={<MainPage />}/>
-            <Route path={appRoutes.COURSE} element={<CoursePage />}/>
-            <Route path={appRoutes.USER_PROFILE} element={<UserProfilePage />}/>
-            <Route path={appRoutes.WORKOUT_VIDEO} element={<WorkoutVideoPage />}/>
-        </Routes>
-    )
-
+  return (
+    <Routes>
+      <Route path={appRoutes.MAIN} element={<MainPage />} />
+      <Route path={appRoutes.COURSE} element={<CoursePage />} />
+      <Route path={appRoutes.USER_PROFILE} element={<UserProfilePage />} />
+      <Route path={appRoutes.WORKOUT_VIDEO} element={<WorkoutVideoPage />} />
+    </Routes>
+  );
 }
--- src/components/DataComponents/FittingTexts.tsx
+++ src/components/DataComponents/FittingTexts.tsx
@@ -1,19 +1,17 @@
 export default function FittingText() {
   return (
-    <div  className="container flex justify-between mx-auto">
+    <div className="container flex justify-between mx-auto">
       <h1>Подойдет для вас, если:</h1>
-      <div  >
+      <div>
         <div className="container flex justify-between mx-auto">
           <div>
-            <h2  className="rounded-button bg-mainColor text-black" >1</h2>
-
+            <h2 className="rounded-button bg-mainColor text-black">1</h2>
             <h2>Давно хотели попробовать йогу, но не решались начать</h2>
           </div>
         </div>
         <div>
           <div>
-
-            <h2  className="rounded-button bg-mainColor text-black" >2</h2>
+            <h2 className="rounded-button bg-mainColor text-black">2</h2>
             <h2>2</h2>
             <h2>
               Хотите укрепить позвоночник, избавиться от болей в спине и
@@ -23,10 +21,11 @@ export default function FittingText() {
         </div>
         <div>
           <div className="">
-            <h2  className="rounded-button bg-mainColor text-black" >3</h2>
-          <div>
-            <h2>3</h2>
-            <h2>Ищете активность, полезную для тела и души</h2>
+            <h2 className="rounded-button bg-mainColor text-black">3</h2>
+            <div>
+              <h2>3</h2>
+              <h2>Ищете активность, полезную для тела и души</h2>
+            </div>
           </div>
         </div>
       </div>
