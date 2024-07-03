a420ece4e5cba91646f5594348568b028db7c602
Igor Shipitko <garikrhip20@gmail.com>
2024-07-03 18:24:45 +0300

    dev1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 /* import './App.css' */
 
+import Header from "./components/Common/Header";
+import Directions from "./components/DataComponents/Directions";
+import FittingText from "./components/DataComponents/FittingTexts";
+
 
 export default function App() {
-  
+    return (
+        <>
+            <Header />
+            <Directions/>
+            <FittingText/>
+        </>
+  )
   
 }
 
---
+++ src/components/DataComponents/FittingTexts.tsx
@@ -0,0 +1,30 @@
+export default function FittingText() {
+  return (
+    <div  className="container flex justify-between mx-auto">
+      <h1>Подойдет для вас, если:</h1>
+      <div  >
+        <div className="container flex justify-between mx-auto">
+          <div>
+            <h2  className="rounded-button bg-mainColor text-black" >1</h2>
+            <h2>Давно хотели попробовать йогу, но не решались начать</h2>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2  className="rounded-button bg-mainColor text-black" >2</h2>
+            <h2>
+              Хотите укрепить позвоночник, избавиться от болей в спине и
+              суставах
+            </h2>
+          </div>
+        </div>
+        <div>
+          <div className="">
+            <h2  className="rounded-button bg-mainColor text-black" >3</h2>
+            <h2>Ищете активность, полезную для тела и души</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
