35b0f317d8393f363ff11cac0c9b747ef88f7603
DKTNS <zigindmitry@gmail.com>
2024-07-03 08:32:03 +0300

    Доабвлены настройки БД

---
+++ src/api/db_config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkN4xe36J9U7gt-sOI3NmKzHpbVpMTcXE",
+  authDomain: "spfitnesspro.firebaseapp.com",
+  databaseURL: "https://spfitnesspro-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "spfitnesspro",
+  storageBucket: "spfitnesspro.appspot.com",
+  messagingSenderId: "646527140879",
+  appId: "1:646527140879:web:9157e41fa4ab53e1fb4f7a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
