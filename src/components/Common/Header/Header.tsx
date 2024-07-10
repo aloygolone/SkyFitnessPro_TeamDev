c1937f34f4d265af8b6e2132aaf176105066d54b
Igor Shipitko <garikrhip20@gmail.com>
2024-07-11 01:00:52 +0300

    dir1 поправил замечания

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -8,9 +8,9 @@ export default function Header() {
     <div className="mx-auto mt-[50px] flex items-center justify-between">
       <div className="flex flex-col space-y-[15px]">
         <div className="">
-        <Link to={appRoutes.COURSE}>
-        <img src="../images/logo.png" alt="logo"/>
-      </Link>
+          <Link to={appRoutes.MAIN}>
+            <img src="../images/logo.png" alt="logo" />
+          </Link>
         </div>
         <div className="space-y-[60px]">
           <p className="font-roboto hidden md:flex">
@@ -18,25 +18,23 @@ export default function Header() {
           </p>
         </div>
       </div>
-      
 
       {/* <button
         className="h-[52px] w-[103px] rounded-buttonRadius bg-mainColor px-btnX py-btnY text-center font-defaultFont font-normal text-black hover:bg-mainHover" 
       >
         Войти
       </button> */}
-     
-      
+
       {/* Если пользователь авторизован, то user Блок */}
       <div>
         <svg className="">
-           <use xlinkHref="public/icons/profile_icon.svg" /> 
+          <use xlinkHref="public/icons/profile_icon.svg" />
         </svg>
 
         <div>
           <div>Сергей</div>
           <svg className="">
-           <use xlinkHref="public/icons/user_arrow_icon.svg" /> 
+            <use xlinkHref="public/icons/user_arrow_icon.svg" />
           </svg>
         </div>
       </div>
