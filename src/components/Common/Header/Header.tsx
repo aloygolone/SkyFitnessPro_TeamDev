717156f3207dd6d2fcb7e42e0826f94c8898e4e6
Igor Shipitko <garikrhip20@gmail.com>
2024-07-12 19:26:13 +0300

    fit  Header привел в первоначальное состояние

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,46 +1,32 @@
-import { Link } from "react-router-dom";
-import "../../../css/style.css";
-
-import UserModal from "../UserModal/UserModal";
-import { appRoutes } from "../../../lib/appRoutes";
+import "../../css/style.css";
 
 export default function Header() {
   return (
-    <div className="container mx-auto">
-      <div className="mx-auto mt-[50px] flex items-center justify-between">
-        <div className="flex flex-col space-y-[15px]">
-          <div className="">
-            <Link to={appRoutes.COURSE}>
-              <img src="/images/logo.png" alt="logo" />
-            </Link>
-          </div>
-          <div className="space-y-[60px]">
-            <p className="font-roboto hidden md:flex">
-              Онлайн тренировки для занятий дома
-            </p>
-          </div>
+    <div className="container flex justify-between mx-auto">
+      <div className="flex flex-col space-y-[15px]">
+        <div>
+          {/* <Link to="/">
+            <img src="" alt="logo">
+          </Link> */}
         </div>
+        <div>
+          <p className="text-black">Онлайн тренировки для занятий дома</p>
+        </div>
+      </div>
 
-        {/* <button
-        className="h-[52px] w-[103px] rounded-buttonRadius bg-mainColor px-btnX py-btnY text-center font-defaultFont font-normal text-black hover:bg-mainHover" 
+      <div
+        className="rounded-button bg-mainColor text-black" /* onClick={handleSigninForm} */
       >
         Войти
-      </button> */}
+      </div>
+      {/* user Блок */}
+      <div>
+        <div>profile_icon</div>
 
-        {/* Если пользователь авторизован, то user Блок */}
         <div>
-          <svg className="">
-            <use xlinkHref="public/icons/profile_icon.svg" />
-          </svg>
-
-          <div>
-            <div>Сергей</div>
-            <svg className="">
-              <use xlinkHref="public/icons/user_arrow_icon.svg" />
-            </svg>
-          </div>
+          <div>Сергей</div>
+          <svg>{/*  <use user_arrow_icon="/" /> */}</svg>
         </div>
-        <UserModal />
       </div>
     </div>
   );
