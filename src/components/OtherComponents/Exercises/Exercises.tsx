ac9f06e14ae0b0ceee43da3ba5d97eed81f08d54
Illia Hryn <grinpis01@gmail.com>
2024-07-15 12:07:07 +0200

    save changes style

--- src/components/OtherComponents/Exercises/Exercises.tsx
+++ src/components/OtherComponents/Exercises/Exercises.tsx
@@ -2,30 +2,33 @@ import "../../../css/style.css";
 
 export default function Exercises() {
   const testArr = [
-    { info: "Наклоны вперед", progress: " 0%" },
-    { info: "Наклоны назад", progress: " 2%" },
-    { info: "Поднятие ног, согнутых в коленях", progress: " 23%" },
-    { info: "Наклоны вперед", progress: " 21%" },
-    { info: "Наклоны вперед", progress: " 42%" },
-    { info: "Наклоны назад", progress: " 3%" },
-    { info: "Поднятие ног, согнутых в коленях", progress: " 12%" },
-    { info: "Поднятие ног, согнутых в коленях", progress: " 13%" },
-    { info: "Поднятие ног, согнутых в коленях", progress: " 23%" },
+    { info: "Наклоны вперед", progress: 90 },
+    { info: "Наклоны назад", progress: 2 },
+    { info: "Поднятие ног, согнутых в коленях", progress: 23 },
+    { info: "Наклоны вперед", progress: 21 },
+    { info: "Наклоны вперед", progress: 42 },
+    { info: "Наклоны назад", progress: 3 },
+    { info: "Поднятие ног, согнутых в коленях", progress: 12 },
+    { info: "Поднятие ног, согнутых в коленях", progress: 63 },
+    { info: "Поднятие ног, согнутых в коленях", progress: 23 },
   ];
   return (
-    <div className="mt-[40px] flex rounded-3xl bg-white">
+    <div className="mt-[40px] flex max-w-[1234px] rounded-3xl bg-white">
       <div className="p-[40px]">
         <h1 className="mb-[20px] text-3xl">Упражнения тренировки 2</h1>
-        <div className="mb-[40px] flex flex-wrap">
+        <div className="mb-[40px] flex flex-wrap justify-between">
           {testArr.map((el, index) => {
             return (
-              <p
-                className="mb-[20px] mr-[160px] h-[20px] w-[320px]"
-                key={index}
-              >
-                {el.info}
-                {el.progress}
-              </p>
+              <div className="mb-[20px] flex flex-col" key={index}>
+                <p className="mb-[10px] h-[20px] w-[320px]">
+                  {el.info} {el.progress}%
+                </p>
+                <progress
+                  className="h-[6px] w-[288px]"
+                  value={el.progress}
+                  max="100"
+                ></progress>
+              </div>
             );
           })}
         </div>
--- src/components/OtherModals/WorkoutModal/WorkoutModal.tsx
+++ src/components/OtherModals/WorkoutModal/WorkoutModal.tsx
@@ -1,7 +1,51 @@
 import "../../../css/style.css";
-import { WorkoutCursList } from "../WorkoutCursList/WorkoutCursList";
 
 export default function WorkoutModal() {
+  const testArr = [
+    {
+      title: "Утренняя практика",
+      description: "Йога на каждый день",
+      day: 1,
+      active: "finish",
+    },
+    {
+      title: "Красота и здоровье",
+      description: "Йога на каждый день",
+      day: 2,
+      active: "finish",
+    },
+    {
+      title: "Асаны стоя",
+      description: "Йога на каждый день",
+      day: 3,
+      active: "start",
+    },
+    {
+      title: "Растягиваем мышцы бедра",
+      description: "Йога на каждый день",
+      day: 4,
+      active: "start",
+    },
+    {
+      title: "Гибкость спины",
+      description: "Йога на каждый день",
+      day: 5,
+      active: "start",
+    },
+    {
+      title: "Гибкость рук",
+      description: "Йога на каждый день",
+      day: 6,
+      active: "start",
+    },
+    {
+      title: "Сальто назад",
+      description: "Йога на каждый день",
+      day: 7,
+      active: "start",
+    },
+  ];
+
   return (
     <div className="flex h-[608px] w-[460px] justify-center rounded-3xl bg-white">
       <div className="w-[384px] p-[40px]">
@@ -13,11 +57,39 @@ export default function WorkoutModal() {
         <div className="h-[446px]">
           <div className="overflow-hidden scroll-smooth">
             <div className="h-[364px] overflow-y-scroll scroll-smooth">
-              <WorkoutCursList />
+              {testArr.map((el, index) => {
+                return (
+                  <div key={index} className="mb-[10px] h-[56px] pb-[8px]">
+                    <div className="border-colorBorderBtn mb-[10px] flex content-center items-center justify-start border-b-[1px]">
+                      {el.active === "finish" ? (
+                        <svg
+                          className="pointer-events-none ml-[2px] mr-[12px] h-[20px] w-[20px]"
+                          key={el.day}
+                        >
+                          <use xlinkHref="/public/icons/sprite.svg#icon-done" />
+                        </svg>
+                      ) : (
+                        <svg
+                          className="ml-[2px] mr-[12px] h-[20px] w-[20px] cursor-pointer"
+                          key={el.day}
+                        >
+                          <use xlinkHref="/public/icons/sprite.svg#icon-done" />
+                        </svg>
+                      )}
+                      <div className="mr-[10px]">
+                        <p className="text-lg">{el.title}</p>
+                        <p>
+                          {el.description} / {el.day} день
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
           <div className="flex content-center items-center justify-center">
-            <button className="mt-[24px] h-[48px] w-[288px] rounded-3xl bg-mainColor text-xl">
+            <button className="h-[48px] w-[288px] rounded-3xl bg-mainColor text-xl">
               Начать
             </button>
           </div>
