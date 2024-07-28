a6311d8e93a072de5d621ab79d25011502107a26
Eugene Alyoshin <aloyolreal@gmail.com>
2024-07-28 14:14:34 +0700

    закомментировал ненужное пока что

--- src/api/userCourses_api.ts
+++ src/api/userCourses_api.ts
@@ -33,84 +33,84 @@ export const deleteMatchedCourse = async (userId: string, courseId: string) => {
   set(ref(database, `users/${userId}/${courseId}`), {});
 };
 
-export const getUserWorkouts = async (userId: string, courseId: string) => {
-  try {
-    const workoutIdsSnapshot = await get(
-      child(ref(database), `courses/${courseId}/workouts`),
-    );
+// export const getUserWorkouts = async (userId: string, courseId: string) => {
+//   try {
+//     const workoutIdsSnapshot = await get(
+//       child(ref(database), `courses/${courseId}/workouts`),
+//     );
 
-    if (workoutIdsSnapshot.exists()) {
-      const workoutIds = workoutIdsSnapshot.val();
-      const workouts = [];
+//     if (workoutIdsSnapshot.exists()) {
+//       const workoutIds = workoutIdsSnapshot.val();
+//       const workouts = [];
 
-      for (const id of workoutIds) {
-        const workoutDataSnapshot = await get(
-          child(ref(database), `workouts/${id}/name`),
-        );
+//       for (const id of workoutIds) {
+//         const workoutDataSnapshot = await get(
+//           child(ref(database), `workouts/${id}/name`),
+//         );
 
-        if (workoutDataSnapshot.exists()) {
-          const progressSnapshot = await get(
-            child(ref(database), `users/${userId}/${courseId}/${id}/done`),
-          );
+//         if (workoutDataSnapshot.exists()) {
+//           const progressSnapshot = await get(
+//             child(ref(database), `users/${userId}/${courseId}/${id}/done`),
+//           );
 
-          if (progressSnapshot.exists()) {
-            workouts.push({
-              name: workoutDataSnapshot.val(),
+//           if (progressSnapshot.exists()) {
+//             workouts.push({
+//               name: workoutDataSnapshot.val(),
 
-              id,
-              progress: progressSnapshot.val(),
-            });
-          }
-        }
-      }
+//               id,
+//               progress: progressSnapshot.val(),
+//             });
+//           }
+//         }
+//       }
 
-      return workouts; // Возвращаем готовый массив тренировок
-    }
+//       return workouts; // Возвращаем готовый массив тренировок
+//     }
 
-    return []; // Возвращаем пустой массив, если workoutIds не существует
-  } catch (e) {
-    console.error(e);
-    return []; // Возвращаем пустой массив в случае ошибки
-  }
-};
+//     return []; // Возвращаем пустой массив, если workoutIds не существует
+//   } catch (e) {
+//     console.error(e);
+//     return []; // Возвращаем пустой массив в случае ошибки
+//   }
+// };
 
-export const getWorkoutById = async (
-  workoutId: string,
-  userId: string,
-  courseId: string,
-) => {
-  let result: WorkoutType | null = null;
+// export const getWorkoutById = async (
+//   workoutId: string,
+//   userId: string,
+//   courseId: string,
+// ) => {
+//   let result: WorkoutType | null = null;
 
-  try {
-    const snapshot = await get(
-      child(ref(database), `users/${userId}/${courseId}/${workoutId}`),
-    );
+//   try {
+//     const snapshot = await get(
+//       child(ref(database), `users/${userId}/${courseId}/${workoutId}`),
+//     );
 
-    if (snapshot.exists()) {
-      result = snapshot.val();
-    }
-  } catch (e) {
-    console.error(e);
-  }
+//     if (snapshot.exists()) {
+//       result = snapshot.val();
+//     }
+//   } catch (e) {
+//     console.error(e);
+//   }
 
-  return result;
-};
+//   return result;
+// };
 
-export const updateExercisesProgress = async (
-  userId: string,
-  courseId: string,
-  workoutId: string,
-  newExercisesList: Record<string, unknown>,
-) => {
-  try {
-    set(
-      ref(database, `users/${userId}/${courseId}/${workoutId}`),
-      newExercisesList,
-    );
-  } catch (e) {
-    console.error(e);
-  }
-};
+// export const updateExercisesProgress = async (
+//   userId: string,
+//   courseId: string,
+//   workoutId: string,
+//   newExercisesList: Record<string, unknown>,
+// ) => {
+//   try {
+//     set(
+//       ref(database, `users/${userId}/${courseId}/${workoutId}`),
+//       newExercisesList,
+//     );
+//   } catch (e) {
+//     console.error(e);
+//   }
+// };
 
 export const fetchAddFavoriteCourseToUser = async (
   userId: string,
