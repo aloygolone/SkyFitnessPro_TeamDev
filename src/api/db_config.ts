aba9e757d61789ebf6f4ceb52c4bb103fc57d041
Eugene Alyoshin <aloyolreal@gmail.com>
2024-07-30 17:44:30 +0700

    новая БД

--- src/api/db_config.ts
+++ src/api/db_config.ts
@@ -3,15 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD535qvIi964HjwG67c_GrEcid4LGJB4ZY",
-  authDomain: "skyfitnesspro-13b90.firebaseapp.com",
+  apiKey: "AIzaSyCUepvc2q7KR5qk8MChCAU-J0aZhS0-zFQ",
+  authDomain: "skyfitnessproteamdev.firebaseapp.com",
   databaseURL:
-    "https://skyfitnesspro-13b90-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "skyfitnesspro-13b90",
-  storageBucket: "skyfitnesspro-13b90.appspot.com",
-  messagingSenderId: "601960710943",
-  appId: "1:601960710943:web:a0ea1c7c6e01a3dd7b662a",
-  measurementId: "G-DBNNNGMNNG",
+    "https://skyfitnessproteamdev-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "skyfitnessproteamdev",
+  storageBucket: "skyfitnessproteamdev.appspot.com",
+  messagingSenderId: "53313572858",
+  appId: "1:53313572858:web:e2fdbf634545a1deca9e43",
 };
 
 export const app = initializeApp(firebaseConfig);
