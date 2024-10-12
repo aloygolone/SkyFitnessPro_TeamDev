28792ad2cd90967b162ff04214fbce9fc6bb4ef4
Eugene Alyoshin <aloyolreal@gmail.com>
2024-10-12 20:49:30 +0700

    правки в userProfile

--- src/components/OtherComponents/UserProfile/UserProfile.tsx
+++ src/components/OtherComponents/UserProfile/UserProfile.tsx
@@ -2,42 +2,51 @@ import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { appRoutes } from "../../../lib/appRoutes";
 import { useUserData } from "../../../hooks/useUserData";
-import { changePassword } from "../../../api/userAuth_api";
 
 type ErrorType = {
-  oldPassword: string;
-  newPassword: string;
-};
+  password: string,
+  repeatPassword: string,
+}
 
 export default function UserProfile() {
   const { user, logout } = useUserData();
 
   const navigate = useNavigate();
 
-  const [passwordData, setPasswordData] = useState<ErrorType>({
-    oldPassword: "",
-    newPassword: "",
-  });
-
-  // const [isMismatchPassword, setIsMismatchPassword] = useState(false);
+  const [passwordData, setPasswordData] = useState<ErrorType>({ password: "", repeatPassword: "" });
 
   const [isChangeMode, setIsChangeMode] = useState(false);
 
+  const [isNotCorrect, setIsNotCorrect] = useState(false);
+
+  const [isNotCorrect1, setIsNotCorrect1] = useState(false);
+
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setPasswordData({
       ...passwordData,
-      [name]: value,
-    });
-  };
+      [name]: value
+    })
+  }
 
   function handleChangePassword() {
     setIsChangeMode(true);
+
   }
 
-  const handleSubmit = async () => {
-    changePassword(passwordData.newPassword).then(() => setIsChangeMode(false));
-  };
+  function handleSubmit() {
+    setIsNotCorrect(false);
+    setIsNotCorrect1(false);
+    if (!passwordData.password.trim() || !passwordData.repeatPassword.trim()) {
+      setIsNotCorrect(true);
+      return;
+    }
+    if (passwordData.password === passwordData.repeatPassword) {
+      setIsNotCorrect1(true);
+      return;
+    }
+    setIsChangeMode(false);
+  }
 
   const handleLogout = () => {
     logout?.();
@@ -48,71 +57,75 @@ export default function UserProfile() {
     <>
       <h2
         data-tid="titleProfile"
-        className="mb-10 ml-8 text-[40px] font-semibold leading-[44px] sm:text-[26px] md:text-[32px]"
+        className="mb-10 ml-8 text-[40px] sm:text-[26px] md:text-[32px] font-semibold leading-[44px]"
       >
         Профиль
       </h2>
       <div
         data-tid="profileUserInfoBlock"
-        className="bg-white-base mb-10 ml-8 mr-8 mt-10 h-[257px] rounded-[30px] p-[30px] shadow-xl sm:mt-6 sm:h-auto md:mt-6"
+        className="mb-10 ml-8 mr-8 bg-white-base mt-10 sm:mt-6 md:mt-6 h-[257px] sm:h-auto p-[30px] rounded-[30px] shadow-xl"
       >
         <div
           data-tid="contentBlock"
-          className="flex flex-row gap-[30px] md:gap-6"
+          className="flex flex-row gap-[30px] md:gap-6 "
         >
           <svg className="">
             <use xlinkHref="./public/icons/sprite.svg#icon-profile-nophoto-full" />
           </svg>
-          <div data-tid="userData" className="flex flex-col gap-[10px]">
-            <h3 className="text-[32px] font-medium leading-[35px] sm:text-[26px]">
+          <div
+            data-tid="userData"
+            className="flex flex-col gap-[10px] "
+          >
+            <h3 className="text-[32px] sm:text-[26px] font-medium leading-[35px]">
               Сергей
             </h3>
             <div className="mt-3 text-[18px] font-normal leading-[19px]">
               Логин: {user?.email}
             </div>
             {!isChangeMode ? (
-              <div className="flex items-center text-[18px] font-normal leading-[19px]">
+              <div className="flex items-center text-[18px] font-normal leading-[19px] ">
                 Пароль:
                 <button
-                  className="w-[120px] rounded-blockRadiusMin border bg-gray-400 text-gray-400 sm:h-[25px]"
-                  type="button"
-                ></button>
+                  className="w-[120px] sm:h-[25px] rounded-blockRadiusMin border bg-gray-400 text-gray-400"
+                >
+                </button>
               </div>
             ) : (
               <>
                 <input
-                  className="rounded-small border-gray-extra bg-white-base text-black-base placeholder-gray-extra mb-2.5 h-[52px] w-[280px] appearance-none rounded-inputRadius border px-[18px] py-[12px] text-lg"
-                  name="oldPassword"
+                  className="mb-2.5 h-[52px] w-[280px] px-[18px] py-[12px] text-lg rounded-inputRadius appearance-none border rounded-small border-gray-extra  bg-white-base text-black-base placeholder-gray-extra"
+                  name="password"
                   type="password"
                   placeholder="Старый пароль"
-                  value={passwordData.oldPassword}
-                  onChange={handleInputChange}
-                />
+                  value={passwordData.password}
+                  onChange={handleInputChange} />
                 <input
-                  className="border-error rounded-small border-gray-extra bg-white-base text-black-base placeholder-gray-extra h-[52px] w-[280px] appearance-none rounded-inputRadius border px-[18px] py-[12px] text-lg"
-                  name="newPassword"
+                  className="border-error h-[52px] w-[280px] px-[18px] py-[12px] rounded-inputRadius text-lg appearance-none border rounded-small border-gray-extra bg-white-base text-black-base placeholder-gray-extra"
+                  name="repeatPassword"
                   type="password"
                   placeholder="Новый пароль"
-                  value={passwordData.newPassword}
-                  onChange={handleInputChange}
-                />
+                  value={passwordData.repeatPassword}
+                  onChange={handleInputChange} />
+                {isNotCorrect && (<div className="text-red-500">Заполните все поля!</div>)}
+                {isNotCorrect1 && (<div className="text-red-500">Пароли не должны совпадать!</div>)}
               </>
             )}
 
-            <div className="mt-5 flex w-[394px] flex-row gap-[10px] sm:items-center">
+            <div className="mt-5 w-[394px] flex flex-row gap-[10px] sm:items-center">
+
               {!isChangeMode ? (
                 <button
                   onClick={handleChangePassword}
-                  className="w-[192px] rounded-buttonRadius border bg-mainColor text-[18px] font-normal leading-[19.8px] hover:bg-mainHover active:bg-black sm:h-[50px]"
-                  type="button"
+                  className="w-[192px] sm:h-[50px] rounded-buttonRadius text-[18px] font-normal leading-[19.8px] 
+                  border  bg-mainColor hover:bg-mainHover active:bg-black"
                 >
                   Изменить пароль
                 </button>
               ) : (
                 <button
                   onClick={handleSubmit}
-                  className="w-[192px] rounded-buttonRadius border bg-mainColor text-[18px] font-normal leading-[19.8px] hover:bg-mainHover active:bg-black sm:h-[50px]"
-                  type="button"
+                  className="w-[192px] sm:h-[50px] rounded-buttonRadius text-[18px] font-normal leading-[19.8px] 
+                  border  bg-mainColor hover:bg-mainHover active:bg-black"
                 >
                   Сохранить
                 </button>
@@ -120,8 +133,9 @@ export default function UserProfile() {
               <Link to={appRoutes.MAIN}>
                 <button
                   onClick={handleLogout}
-                  className="w-[192px] rounded-buttonRadius border border-zinc-900 text-[18px] font-normal leading-[19.8px] hover:bg-bgColor active:bg-blackout sm:h-[50px]"
-                  type="button"
+                  className="w-[192px] sm:h-[50px] rounded-buttonRadius 
+                text-[18px] font-normal leading-[19.8px] 
+                border hover:bg-bgColor active:bg-blackout border-zinc-900"
                 >
                   Выйти
                 </button>
